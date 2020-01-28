08f8f6bcf9c26234c2ddd6818e4c6db4b3488e46
chsong <gramryn@gmail.com>
2020-01-28 17:01:52 +0900

    Create Jenkinsfile.groovy

---
+++ sampleapps/Jenkinsfile.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    environment {
+        
+        registry = "spring-webapp"
+        registryCredential = 'private-docker-reg'
+    }
+    agent any
+    stages {
+        stage('Environment') {
+            parallel {
+                stage('docker login') {
+                    steps {
+                        sh 'sudo docker login dockerregistry:5000'
+                    }
+                }
+                stage('docker image lists') {
+                    steps {
+                        sh 'sudo docker images'
+                    }
+                }
+            }
+        }
+        stage('Build Jar') {
+            steps {
+                sh 'mvn clean install'
+            }
+        }
+        stage('Build docker image') {
+            steps {
+                sh 'docker build -t $registry:latest .'
+            }
+        }
+        stage('Deploy docker image') {
+            steps {
+                withDockerRegistry([ credentialsId: registryCredential, url: "" ]) {
+                    sh 'docker push $registry:latest'
+                }
+            }
+        }
+        stage('Clean docker image') {
+            steps{
+                sh "docker rmi $registry"
+            }
+        }
+     }
+    
+}
